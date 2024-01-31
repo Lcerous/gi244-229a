9d5d450f46ae9003a2b38431a91e23e7fb092917
Lcerous <148931050+Lcerous@users.noreply.github.com>
2024-01-31 10:47:10 +0700

    VFX when move

--- Assets/Scripts/Command/UnitCommand.cs
+++ Assets/Scripts/Command/UnitCommand.cs
@@ -43,6 +43,7 @@ public class UnitCommand : MonoBehaviour
     private void CommandToGround(RaycastHit hit, Unit unit)
     {
         UnitsMoveToPosition(hit.point, unit);
+        CreateVFXMarker(hit.point, MainUI.instance.SelectionMarker);
     }
 
     private void TryCommand(Vector2 screenPos)
@@ -61,6 +62,15 @@ public class UnitCommand : MonoBehaviour
             }
         }
     }
+
+    private void CreateVFXMarker(Vector3 pos, GameObject vfxPrefab)
+    {
+        if (vfxPrefab == null)
+            return;
+
+        Instantiate(vfxPrefab, new Vector3(pos.x, 0.1f, pos.z), Quaternion.identity);
+    }
+
 
 
 }
