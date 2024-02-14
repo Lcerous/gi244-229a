5efd9b72c435712b5f28f4fdb2868d9ae7ce3800
Lcerous <148931050+Lcerous@users.noreply.github.com>
2024-02-14 10:27:45 +0700

    addcode

--- Assets/Scripts/Command/UnitSelect.cs
+++ Assets/Scripts/Command/UnitSelect.cs
@@ -14,6 +14,10 @@ public class UnitSelect : MonoBehaviour
     private Unit curUnit; //current selected single unit
     public Unit CurUnit { get { return curUnit; } }
 
+    [SerializeField]
+    private Building curBuilding; //current selected single building
+    public Building CurBuilding { get { return curBuilding; } }
+
     private Camera cam;
     private Faction faction;
 
@@ -85,6 +89,10 @@ public class UnitSelect : MonoBehaviour
                 case "Unit":
                     SelectUnit(hit);
                     break;
+                
+                case "Building"
+                    BuildingSelect(hit);
+                    break;
             }
         }
     }
@@ -94,12 +102,15 @@ public class UnitSelect : MonoBehaviour
     {
         if (curUnit != null)
             curUnit.ToggleSelectionVisual(false);
+        if (curBuilding != null) 
+            curBuilding.ToggleSelectionVisual(false);
     }
 
     private void ClearEverything()
     {
         ClearAllSelectionVisual();
         curUnit = null;
+        curBuilding = null;
 
         //Clear UI
         InfoManager.instance.ClearAllInfo();
@@ -110,9 +121,24 @@ public class UnitSelect : MonoBehaviour
         InfoManager.instance.ShowAllInfo(u);
     }
 
-    [SerializeField]
-    private Building curBuilding; //current selected single building
-    public Building CurBuilding { get { return curBuilding; } }
+    private void ShowBuilding(Building b)
+    {
+        InfoManager.instance.ShowAllInfo(b);
+    }
+
+    private void BuildingSelect(RaycastHit hit)
+    {
+        curBuilding = hit.collider.GetComponent<Building>();
+        curBuilding.ToggleSelectionVisual(true);
+
+        if (GameManager.instance.MyFaction.IsMyBuilding(curBuilding))
+        {
+            //Debug.Log("my building");
+            ShowBuilding(curBuilding);//Show building info
+        }
+    }
+
+
 
 
 
