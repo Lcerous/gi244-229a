e7cd94e2e22ff074a1bd523187651a578b58cd00
Lcerous <148931050+Lcerous@users.noreply.github.com>
2024-04-01 21:13:21 +0700

    del/store rec

--- Assets/Scripts/Faction/Faction.cs
+++ Assets/Scripts/Faction/Faction.cs
@@ -20,7 +20,7 @@ public class Faction : MonoBehaviour
     }
 
     [SerializeField] private Nation nation;
-    //remember this "Fation" is Nation
+    
     public Nation Fation {  get { return nation; } }
 
     [SerializeField] private Transform unitsParent;
@@ -32,6 +32,9 @@ public class Faction : MonoBehaviour
     [SerializeField] private Transform ghostBuildingParent;
     public Transform GhostBuildingParent { get { return ghostBuildingParent; } }
 
+    [SerializeField]
+    private Transform startPosition; //start position for Faction
+    public Transform StartPosition { get { return startPosition; } }
 
 
     public Nation GetNation()
@@ -63,6 +66,8 @@ public class Faction : MonoBehaviour
 
     [SerializeField] private List<Building> aliveBuildings = new List<Building>();
     public List<Building> AliveBuildings { get { return aliveBuildings; } }
+
+    
 
 
 
@@ -140,7 +145,37 @@ public class Faction : MonoBehaviour
         stone -= building.StructureCost.stone;
     }
 
+    public Vector3 GetHQSpawnPos()
+    {
+        foreach (Building b in aliveBuildings)
+        {
+            if (b.IsHQ)
+                return b.SpawnPoint.position;
+        }
+        return startPosition.position;
+    }
 
+    public void GainResource(ResourceType resourceType, int amount)
+    {
+        switch (resourceType)
+        {
+            case ResourceType.Food:
+                food += amount;
+                break;
+            case ResourceType.Wood:
+                wood += amount;
+                break;
+            case ResourceType.Gold:
+                gold += amount;
+                break;
+            case ResourceType.Stone:
+                stone += amount;
+                break;
+        }
+
+        if (this == GameManager.instance.MyFaction)
+            MainUI.instance.UpdateAllResource(this);
+    }
 
 
 
--- Assets/Scripts/Unit/Worker.cs
+++ Assets/Scripts/Unit/Worker.cs
@@ -48,6 +48,14 @@ public class Worker : MonoBehaviour
             case UnitState.Gather:
                 GatherUpdate();
                 break;
+
+            case UnitState.DeliverToHQ:
+                DeliverToHQUpdate();
+                break;
+
+            case UnitState.StoreAtHQ: 
+                StoreAtHQUpdate();
+                break;
         }
     }
 
@@ -100,6 +108,34 @@ public class Worker : MonoBehaviour
             }
             else //amount is full, go back to deliver at HQ
                 unit.SetState(UnitState.DeliverToHQ);
+        }
+    }
+
+    private void DeliverToHQUpdate()
+    {
+        if (Time.time - unit.LastPathUpdateTime > unit.PathUpdateRate)
+        {
+            unit.LastPathUpdateTime = Time.time;
+
+            unit.NavAgent.SetDestination(unit.Faction.GetHQSpawnPos());
+            unit.NavAgent.isStopped = false;
+        }
+
+        if (Vector3.Distance(transform.position, unit.Faction.GetHQSpawnPos()) <= 1f)
+            unit.SetState(UnitState.StoreAtHQ);
+    }
+
+    private void StoreAtHQUpdate()
+    {
+        unit.LookAt(unit.Faction.GetHQSpawnPos());
+
+        if (amountCarry > 0)
+        {
+            // Deliver the resource to Faction
+            unit.Faction.GainResource(carryType, amountCarry);
+            amountCarry = 0;
+
+            //Debug.Log("Delivered");
         }
     }
 
