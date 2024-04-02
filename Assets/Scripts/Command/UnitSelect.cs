dcc79e254de8907b54b096113aa919e6828a8524
Lcerous <148931050+Lcerous@users.noreply.github.com>
2024-04-02 21:24:21 +0700

    need to fix

--- Assets/Scripts/Command/UnitSelect.cs
+++ Assets/Scripts/Command/UnitSelect.cs
@@ -12,8 +12,8 @@ public class UnitSelect : MonoBehaviour
     private LayerMask layerMask;
 
     [SerializeField]
-    private Unit curUnit; //current selected single unit
-    public Unit CurUnit { get { return curUnit; } }
+    private List<Unit> curUnits = new List<Unit>(); 
+    public List<Unit> CurUnits { get { return curUnits; } }
 
     [SerializeField]
     private Building curBuilding; //current selected single building
@@ -33,7 +33,10 @@ public class UnitSelect : MonoBehaviour
     [SerializeField]
     private ResourceSource curResource; //current selected resource
 
-
+    [SerializeField]
+    private RectTransform selectionBox;
+    private Vector2 oldAnchoredPos;//Box old anchored position
+    private Vector2 startPos;//point where mouse is down
 
     void Awake()
     {
@@ -49,6 +52,8 @@ public class UnitSelect : MonoBehaviour
         cam = Camera.main;
         layerMask = LayerMask.GetMask("Unit", "Building", "Resource", "Ground");
 
+        selectionBox = MainUI.instance.SelectionBox;
+
         instance = this;
     }
 
@@ -58,6 +63,8 @@ public class UnitSelect : MonoBehaviour
         //mouse down
         if (Input.GetMouseButtonDown(0))
         {
+            startPos = Input.mousePosition;
+
             //if click UI, don't clear
             if (EventSystem.current.IsPointerOverGameObject())
             {
@@ -66,9 +73,16 @@ public class UnitSelect : MonoBehaviour
             ClearEverything();
         }
 
+        // mouse held down
+        if(Input.GetMouseButton(0))
+        {
+            UpdateSelectionBox(Input.mousePosition);
+        }
+
         // mouse up
         if (Input.GetMouseButtonUp(0))
         {
+            ReleaseSelectionBox(Input.mousePosition);
             TrySelect(Input.mousePosition);
         }
 
@@ -76,15 +90,16 @@ public class UnitSelect : MonoBehaviour
 
     private void SelectUnit(RaycastHit hit)
     {
-        curUnit = hit.collider.GetComponent<Unit>();
+        Unit unit = hit.collider.GetComponent<Unit>();
 
-        curUnit.ToggleSelectionVisual(true);
 
         Debug.Log("Selected Unit");
 
-        if (GameManager.instance.MyFaction.IsMyUnit(curUnit)) 
+        if (GameManager.instance.MyFaction.IsMyUnit(unit)) 
         {
-            ShowUnit(curUnit);
+            curUnits.Add(unit);
+            unit.ToggleSelectionVisual(true);
+            ShowUnit(unit);
         }
     }
 
@@ -130,8 +145,8 @@ public class UnitSelect : MonoBehaviour
 
     private void ClearAllSelectionVisual()
     {
-        if (curUnit != null)
-            curUnit.ToggleSelectionVisual(false);
+        foreach (Unit u in curUnits)
+            u.ToggleSelectionVisual(false);
         if (curBuilding != null) 
             curBuilding.ToggleSelectionVisual(false);
         if (curResource != null)
@@ -141,7 +156,10 @@ public class UnitSelect : MonoBehaviour
     private void ClearEverything()
     {
         ClearAllSelectionVisual();
-        curUnit = null;
+
+        curUnits.Clear();
+
+        curUnits = null;
         curBuilding = null;
 
         //Clear UI
@@ -179,6 +197,48 @@ public class UnitSelect : MonoBehaviour
         ShowResource();//Show resource info
     }
 
+    private void UpdateSelectionBox(Vector3 mousePos)
+    {
+        //Debug.Log("Mouse Pos - " + curMousePos);
+        if (!selectionBox.gameObject.activeInHierarchy && curBuilding == null)
+            selectionBox.gameObject.SetActive(true);
+
+        float width = mousePos.x - startPos.x;
+        float height = mousePos.y - startPos.y;
+
+        selectionBox.sizeDelta = new Vector2(Mathf.Abs(width), Mathf.Abs(height));
+        selectionBox.anchoredPosition = startPos + new Vector2(width / 2, height / 2);
+
+        //store old position for real unit selection
+        oldAnchoredPos = selectionBox.anchoredPosition;
+    }
+
+    private void ReleaseSelectionBox(Vector2 mousePos)
+    {
+        //Debug.Log("Step 2 - " + _doubleClickMode);
+        Vector2 min; //down-left corner
+        Vector2 max; //top-right corner
+
+        selectionBox.gameObject.SetActive(false);
+
+        min = oldAnchoredPos - (selectionBox.sizeDelta / 2);
+        max = oldAnchoredPos + (selectionBox.sizeDelta / 2);
+
+        //Debug.Log("min = " + min);
+        //Debug.Log("max = " + max);
+
+        foreach (Unit unit in GameManager.instance.MyFaction.AliveUnits)
+        {
+            Vector2 unitPos = cam.WorldToScreenPoint(unit.transform.position);
+
+            if (unitPos.x > min.x && unitPos.x < max.x && unitPos.y > min.y && unitPos.y < max.y)
+            {
+                curUnits.Add(unit);
+                unit.ToggleSelectionVisual(true);
+            }
+        }
+        selectionBox.sizeDelta = new Vector2(0, 0); //clear Selection Box's size;
+    }
 
 
 
